94a57443b8fe3eddc68d99f9e3b272f9cb1b1a9f
Mustafa <mustafa.a.jama@gmail.com>
2018-12-11 16:49:36 -0500

    just double checkin'

--- app/Services/Validation/Rules/ApplicantHasRelationRule.php
+++ app/Services/Validation/Rules/ApplicantHasRelationRule.php
@@ -1,46 +1,56 @@
-<?php
+]<?php
 
-namespace App\Services\Validation\Rules;
+namespace App\Notifications;
 
-use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Lang;
-use App\Models\Applicant;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
 
-class ApplicantHasRelationRule implements Rule
+class ResetPasswordNotification extends Notification
 {
-
-    protected $applicant;
-    protected $relation;
+    use Queueable;
 
     /**
-     * Create a new rule instance.
+     * The password reset token.
      *
-     * @param  App\Models\Applicant  $applicant
-     * @param  string  $relation
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
      * @return void
      */
-    public function __construct($applicant, $relation)
+    public function __construct($token)
     {
-        $this->applicant = $applicant;
-        $this->relation = $relation;
+        $this->token = $token;
     }
 
     /**
-     * This check passes if the model's relation contains an object whose id=$value
-     * @param  [type] $attribute [description]
-     * @param  [type] $value     [description]
-     * @return [type]            [description]
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
      */
-    public function passes($attribute, $value)
+    public function via($notifiable)
     {
-        return $this->applicant->getRelationValue($this->relation)
-            ->pluck('id')->contains($value);
+        return ['mail'];
     }
 
-    public function message()
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
     {
-        return Lang::get('validation.custom.applicant_has_relation');
+        return (new MailMessage)
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', url(route('password.reset', $this->token, false)))
+            ->line('If you did not request a password reset, no further action is required.');
     }
 }
