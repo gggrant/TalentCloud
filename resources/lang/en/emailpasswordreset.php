eeac7f719e08446dcd973c1c2fd1858cb246aeb0
Mustafa <mustafa.a.jama@gmail.com>
2018-12-11 16:43:18 -0500

    eng lang file

--- resources/lang/en/emailpasswordreset.php
+++ resources/lang/en/emailpasswordreset.php
@@ -1,8 +1,17 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Email Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during password reset emails for 
+    | the user. You are free to modify these language lines according to your 
+    | application's requirements.
+    |* 
+    |*  
+    */
+    
+    'line_1'  => 'You are receiving this email because we received a password reset request for your account.',
+    'line_2' => 'If you did not request a password reset, no further action is required.',
+];
